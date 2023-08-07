60dccbf2a567ce9ea6c9b51e6de87d8c33b498d2
Konstantin Kravtsov <kravtsov.konst@gmail.com>
2023-08-07 23:28:29 +0300

    Added different game types buttons in start window

--- src/TicTacToe/Assets/XO/Window/Windows/Start/StartWindow.cs
+++ src/TicTacToe/Assets/XO/Window/Windows/Start/StartWindow.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine.UI;
+﻿using UnityEngine.Serialization;
+using UnityEngine.UI;
 using XO.Modules.Machine;
 using XO.Modules.States;
 using Zenject;
@@ -9,7 +10,10 @@ namespace XO.Window.Windows.Start
   {
     private const string GameScene = "GameScene";
     
-    public Button Game;
+    public Button PlayerVsComputer;
+    public Button PlayerVsPlayer;
+    public Button ComputerVsComputer;
+    
     public Button Back;
 
     private StateMachine _stateMachine;
@@ -25,13 +29,19 @@ namespace XO.Window.Windows.Start
     private void Awake()
     {
       Back.onClick.AddListener(CloseWindow);
-      Game.onClick.AddListener(MoveToFight);
+      
+      PlayerVsComputer.onClick.AddListener(MoveToFight);
+      PlayerVsPlayer.onClick.AddListener(MoveToFight);
+      ComputerVsComputer.onClick.AddListener(MoveToFight);
     }
 
     private void OnDestroy()
     {
       Back.onClick.RemoveListener(CloseWindow);
-      Game.onClick.RemoveListener(MoveToFight);
+      
+      PlayerVsComputer.onClick.RemoveListener(MoveToFight);
+      PlayerVsPlayer.onClick.RemoveListener(MoveToFight);
+      ComputerVsComputer.onClick.RemoveListener(MoveToFight);
     }
 
     private void CloseWindow() => 
