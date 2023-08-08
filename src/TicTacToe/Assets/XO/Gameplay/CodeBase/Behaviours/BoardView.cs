e6ae277144d26d3951c133745c6bb5d084bdf35c
Konstantin Kravtsov <kravtsov.konst@gmail.com>
2023-08-09 00:32:49 +0300

    Created Board And Cell view, added it in scene

---
+++ src/TicTacToe/Assets/XO/Gameplay/CodeBase/Behaviours/BoardView.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using XO.Modules.States;
+using Zenject;
+
+namespace XO.Gameplay.CodeBase.Behaviours
+{
+  public class BoardView : MonoBehaviour
+  {
+    private GameLoop _gameLoop;
+
+    public List<CellView> CellViews;
+
+    [Inject]
+    public void SetDependency(GameLoop gameLoop) => 
+      _gameLoop = gameLoop;
+    
+    private void Awake()
+    {
+      int index = 0;
+      for (var row = 0; row < Board.Size; row++)
+      {
+        for (var column = 0; column < Board.Size; column++)
+        {
+          CellViews[index++].Initialize(row, column);
+        }
+      }
+    }
+  }
+}
---
+++ src/TicTacToe/Assets/XO/Gameplay/CodeBase/Behaviours/CellView.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace XO.Gameplay.CodeBase.Behaviours
+{
+  public class CellView : MonoBehaviour, IPointerClickHandler
+  {
+    private int _row;
+    private int _column;
+
+    public void Initialize(int row, int column)
+    {
+      _row = row;
+      _column = column;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+      Debug.Log($"{_row} {_column}");
+    }
+  }
+}
