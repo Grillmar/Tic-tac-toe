95d83e17f889960979a96303491a184a13501122
Konstantin Kravtsov <kravtsov.konst@gmail.com>
2023-08-09 13:40:32 +0300

    Added back and reload button

---
+++ src/TicTacToe/Assets/XO/Gameplay/CodeBase/Behaviours/BackButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using XO.Modules.Machine;
+using XO.Modules.States;
+using Zenject;
+
+namespace XO.Gameplay.CodeBase.Behaviours
+{
+  public class BackButton : MonoBehaviour
+  {
+    public Button Button;
+    private StateMachine _stateMachine;
+    
+    [Inject]
+    public void SetDependency(StateMachine stateMachine) => 
+      _stateMachine = stateMachine;
+
+    private void Start() => 
+      Button.onClick.AddListener(Back);
+
+    private void OnDestroy() => 
+      Button.onClick.RemoveListener(Back);
+
+    private void Back() => 
+      _stateMachine.Enter<LoadMainState>();
+  }
+}
