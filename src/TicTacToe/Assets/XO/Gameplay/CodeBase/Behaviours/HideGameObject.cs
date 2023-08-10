6bb98984edee833c4b2ec54cbfd2fa60e7e3e281
Konstantin Kravtsov <kravtsov.konst@gmail.com>
2023-08-10 16:07:14 +0300

    Hide buttons if it not player vs computer

---
+++ src/TicTacToe/Assets/XO/Gameplay/CodeBase/Behaviours/HideGameObject.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using UnityEngine;
+using XO.Gameplay.CodeBase.Player;
+using Zenject;
+
+namespace XO.Gameplay.CodeBase.Behaviours
+{
+  public class HideGameObject : MonoBehaviour
+  {
+    public GameObject GameObject;
+    
+    private GameData _gameData;
+
+    [Inject]
+    public void SetDependency(GameData gameData) =>
+      _gameData = gameData;
+
+    private void Start() => 
+      GameObject.SetActive(IsPlayerVsComputer());
+
+    private bool IsPlayerVsComputer() => 
+      _gameData.Players.Any(x=> x == PlayerType.RealPlayer) 
+      && _gameData.Players.Any(x=> x != PlayerType.RealPlayer);
+  }
+}
