72b36abc962933870f2fc201b24779f83992efad
Konstantin Kravtsov <kravtsov.konst@gmail.com>
2023-08-10 12:03:02 +0300

    Change logic create bundle, and rename image

--- src/TicTacToe/Assets/XO/Editor/AssetBuilder.cs
+++ src/TicTacToe/Assets/XO/Editor/AssetBuilder.cs
@@ -10,7 +10,7 @@ public class AssetBuilder : EditorWindow
   private Texture2D _oSprite;
 
   private Texture2D _backgroundSprite;
-  private int _space = 40;
+  private readonly int _space = 40;
 
   private void OnGUI()
   {
@@ -47,14 +47,42 @@ public class AssetBuilder : EditorWindow
   {
     if (_xSprite == null && _oSprite == null && _xSprite == null)
     {
-      Debug.LogError("No image selected!");
+      Debug.LogError("No images selected!");
       return;
     }
 
-    string outputPath = Path.Combine(Application.streamingAssetsPath, _assetBundleName + ".assetbundle");
+    PrepareAsset(_xSprite, "X");
+    PrepareAsset(_oSprite, "O");
+    PrepareAsset(_backgroundSprite, "Background");
+    
+    AssetBundleBuild[] builds = CreateAssetBundleBuilds();
 
-    BuildPipeline.BuildAssetBundle(null, new Object[] { _xSprite, _oSprite, _backgroundSprite }, outputPath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    AssetDatabase.Refresh();
+    
+    BuildPipeline.BuildAssetBundles(Application.streamingAssetsPath, builds, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
 
+    string outputPath = Path.Combine(Application.streamingAssetsPath, _assetBundleName);
     Debug.Log("Asset Bundle created at: " + outputPath);
+    
+    AssetDatabase.Refresh();
+  }
+
+  private void PrepareAsset(Object asset, string name)
+  {
+    string originalImagePath = AssetDatabase.GetAssetPath(asset);
+
+    string newImageName = name + ".png";
+
+    string newImagePath = Path.Combine(Path.GetDirectoryName(originalImagePath), newImageName);
+    
+    AssetDatabase.MoveAsset(originalImagePath, newImagePath);
+  }
+
+  private AssetBundleBuild[] CreateAssetBundleBuilds()
+  {
+    var builds = new AssetBundleBuild[1];
+    builds[0].assetNames = new[] { AssetDatabase.GetAssetPath(_xSprite), AssetDatabase.GetAssetPath(_oSprite), AssetDatabase.GetAssetPath(_backgroundSprite) };
+    builds[0].assetBundleName = _assetBundleName;
+    return builds;
   }
 }
